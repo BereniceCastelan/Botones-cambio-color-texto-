d192ef4cb3a0552449f248116f22d13cb33858ce
Zamarripa <eli.dur.zama@gmail.com>
2023-04-12 13:28:23 -0600

    Initial Commit

---
+++ JATZIRI BERENICE/JATZIRI_BERENICEApp.swift
@@ -0,0 +1,17 @@
+//
+//  JATZIRI_BERENICEApp.swift
+//  JATZIRI BERENICE
+//
+//  Created by CEDAM09 on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct JATZIRI_BERENICEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
